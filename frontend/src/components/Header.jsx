b6206739c8de13e5a6aca28a6ebbfc8fc16f5849
Serviço <delei@pop-os.localdomain>
2021-07-09 16:50:50 -0300

    responsividade

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Style from './Header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
+import { faAlignJustify, faBookOpen } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 
 const Header = ( ) => {
 
+    function clique(e){
+        const menu = document.querySelector(`.${Style.link}`)
+
+        if(menu.classList.contains(`${Style.sumir}`)){
+            menu.classList.remove(`${Style.sumir}`)
+            menu.classList.add(`${Style.aparecer}`)
+            
+        }else{
+            menu.classList.remove(`${Style.aparecer}`)
+            menu.classList.add(`${Style.sumir}`)
+        }
+       
+
+        
+    }
+
     return (
+    <   >
+
         <header className = {Style.header} >
+
             <Link to = "/" className = {Style.logo} >
                 <FontAwesomeIcon className = "fa-4x" icon = {faBookOpen} />
                 <h1>Book Finder</h1>
@@ -17,6 +36,25 @@ const Header = ( ) => {
                 <Link to = 'pesquisar' >Pesquisar</Link>
             </div>
         </header>
+
+        <header className = {Style.mob}>
+             <div className ={Style.mobile} >
+                 <div>
+                    <Link to = "/" className = {Style.logoMobile} >
+                            <FontAwesomeIcon className = "fa-4x" icon = {faBookOpen} />
+                            <h1>Book Finder</h1>
+                    </Link>
+                    <FontAwesomeIcon onClick = {e => clique(e)} icon = {faAlignJustify} size = "2x" className = {Style.menu} />
+                 </div>
+                
+                <div className = {`${Style.link} , ${Style.sumir}`}>
+                    <Link className = ''  to =  '/' >Home</Link>
+                    <Link className = '' to = '/pesquisar' >Pesquisar</Link>
+                </div>
+           </div>
+        </header>
+
+        </>
     )
 }
 
